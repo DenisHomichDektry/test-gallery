4a1877078ef32821ff1079bdc6414bda07fda55a
Denis Homich <dzianis.khomich@dektry.com>
2023-05-10 18:35:56 +0200

    adds switch between gallery and carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import { Carousel } from './components/Carousel';
+import { Gallery } from './components/Gallery';
 
 function App() {
+  const [isGallery, setIsGallery] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setIsGallery(!isGallery)}>Show {isGallery ? 'Carousel': 'Gallery'}</button>
+      {isGallery ? <Gallery/> : <Carousel/>}
     </div>
   );
 }
